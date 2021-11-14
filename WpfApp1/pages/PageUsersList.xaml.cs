4689cce96bc56a55cbf05c4f31784f437b22d7a4
svetlana.28.02 <svetlana.28.02@mail.ru>
2021-11-14 16:56:06 +0300

    картинки и страницы

--- WpfApp1/pages/PageUsersList.xaml.cs
+++ WpfApp1/pages/PageUsersList.xaml.cs
@@ -22,6 +22,8 @@ namespace WpfApp1.pages
     {
         List<users> users;
         List<users> listUsers;
+        PageChange pc = new PageChange();
+
         public PageUsersList()
         {
             InitializeComponent();
@@ -33,6 +35,7 @@ namespace WpfApp1.pages
             cbGenderS.DisplayMemberPath = "gender";
             btnNewUser.Content = "Создать\nнового\nпользователя";
             listUsers = users;
+            DataContext = pc;
         }
         private void lbTraits_Loaded(object sender, RoutedEventArgs e)
         {
@@ -74,6 +77,7 @@ namespace WpfApp1.pages
                 listUsers = listUsers.Where(x => x.gender == Convert.ToInt32(cbGenderS.SelectedValue)).ToList();
             }
             lbUsersList.ItemsSource = listUsers;
+            pc.Countlist = listUsers.Count;
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
@@ -85,42 +89,6 @@ namespace WpfApp1.pages
             tbOT.Text = "";
             tbDO.Text = "";
         }
-        //private void ComboBox_Selected(object sender, RoutedEventArgs e)
-        //{
-        //    if (cbSort.SelectedIndex==0)
-        //    {
-        //        gbOT.Visibility = Visibility.Visible;
-        //        gbDO.Visibility = Visibility.Visible;
-        //        gbName.Visibility = Visibility.Collapsed;
-        //        gbGen.Visibility = Visibility.Collapsed;
-        //        gbBD.Visibility = Visibility.Collapsed;
-        //    }
-        //    if (cbSort.SelectedIndex == 1)
-        //    {
-        //        gbOT.Visibility = Visibility.Collapsed;
-        //        gbDO.Visibility = Visibility.Collapsed;
-        //        gbName.Visibility = Visibility.Visible;
-        //        gbGen.Visibility = Visibility.Collapsed;
-        //        gbBD.Visibility = Visibility.Collapsed;
-        //    }
-        //    if (cbSort.SelectedIndex == 2)
-        //    {
-        //        gbOT.Visibility = Visibility.Collapsed;
-        //        gbDO.Visibility = Visibility.Collapsed;
-        //        gbName.Visibility = Visibility.Collapsed;
-        //        gbGen.Visibility = Visibility.Visible;
-        //        gbBD.Visibility = Visibility.Collapsed;
-        //    }
-        //    if (cbSort.SelectedIndex == 3)
-        //    {
-        //        gbOT.Visibility = Visibility.Collapsed;
-        //        gbDO.Visibility = Visibility.Collapsed;
-        //        gbName.Visibility = Visibility.Collapsed;
-        //        gbGen.Visibility = Visibility.Collapsed;
-        //        gbBD.Visibility = Visibility.Visible;
-        //    }
-
-        //}
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
@@ -151,133 +119,68 @@ namespace WpfApp1.pages
         {
             LoadPages.MainFrame.GoBack();
         }
-        int CurrentPages = 1;
+        
         private void GoPage_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            try
+            TextBlock tb = (TextBlock)sender;//определяем, какой текстовый блок был нажат           
+            //изменение номера страници при нажатии на кнопку
+            switch (tb.Uid)
             {
-                List<users> lb = users.ToList();
-                TextBlock tb = (TextBlock)sender;
-                int CountZapInList = users.Count;
-                int CountZapOnPage = Convert.ToInt32(txtPageCount.Text);
-                int CountPage;
-                if (CountZapOnPage % 2 == 0)
-                {
-                    CountPage = (CountZapInList / CountZapOnPage);
-                }
-                else
-                {
-                    CountPage = (CountZapInList / CountZapOnPage) + 1;
-                }
-                switch (tb.Uid)
-                {
-                    case "prev":
-                        CurrentPages--;
-                        break;
-                    case "1":
-                        if (CurrentPages < 3) CurrentPages = 1;
-                        else if (CurrentPages > CountPage) CurrentPages = CountPage - 4;
-                        else CurrentPages -= 2;
-                        break;
-                    case "2":
-                        if (CurrentPages < 3) CurrentPages = 2;
-                        else if (CurrentPages > CountPage) CurrentPages = CountPage - 3;
-                        else CurrentPages -= 1;
-                        break;
-                    case "3":
-                        if (CurrentPages < 3) CurrentPages = 3;
-                        else if (CurrentPages > CountPage) CurrentPages = CountPage - 2;
-                        break;
-                    case "4":
-                        if (CurrentPages < 3) CurrentPages = 4;
-                        else if (CurrentPages > CountPage) CurrentPages = CountPage - 1;
-                        else CurrentPages++;
-                        break;
-                    case "5":
-                        if (CurrentPages < 3) CurrentPages = 5;
-                        else if (CurrentPages > CountPage) CurrentPages = CountPage;
-                        else CurrentPages += 2;
-                        break;
-                    case "next":
-                        CurrentPages++;
-                        break;
-                    default:
-                        CurrentPages = 1;
-                        break;
-                }
-
-                if (CurrentPages < 1) CurrentPages = 1;
-                if (CurrentPages > CountPage) CurrentPages = CountPage;
-                //отрисовка
-                if (CountPage < 5)
-                {
-                    txt5.Visibility = Visibility.Collapsed;
-                }
-                else if (CountPage < 4)
-                {
-                    txt4.Visibility = Visibility.Collapsed;
-                }
-                else if (CountPage < 3)
-                {
-                    txt3.Visibility = Visibility.Collapsed;
-                }
-                else if (CountPage < 2)
-                {
-                    txt2.Visibility = Visibility.Collapsed;
-                }
-                else
-                {
-                    txt2.Visibility = Visibility.Visible;
-                    txt3.Visibility = Visibility.Visible;
-                    txt4.Visibility = Visibility.Visible;
-                    txt5.Visibility = Visibility.Visible;
-                }
-                if (CurrentPages < 3)
-                {
-                    txt1.Text = " 1 ";
-                    txt2.Text = " 2 ";
-                    txt3.Text = " 3 ";
-                    txt4.Text = " 4 ";
-                    txt5.Text = " 5 ";
-                }
-                else if (CurrentPages > CountPage - 2)
-                {
-                    txt1.Text = " " + (CountPage - 4).ToString() + " ";
-                    txt2.Text = " " + (CountPage - 3).ToString() + " ";
-                    txt3.Text = " " + (CountPage - 2).ToString() + " ";
-                    txt4.Text = " " + (CountPage - 1).ToString() + " ";
-                    txt5.Text = " " + (CountPage).ToString() + " ";
-                }
-                else
-                {
-                    txt1.Text = " " + (CurrentPages - 2).ToString() + " ";
-                    txt2.Text = " " + (CurrentPages - 1).ToString() + " ";
-                    txt3.Text = " " + (CurrentPages).ToString() + " ";
-                    txt4.Text = " " + (CurrentPages + 1).ToString() + " ";
-                    txt5.Text = " " + (CurrentPages + 2).ToString() + " ";
-
-                }
-                txtCurentPage.Text = "Стр: " + (CurrentPages).ToString();
-
-                lb = users.Skip(CurrentPages * CountZapOnPage - CountZapOnPage).Take(CountZapOnPage).ToList();
-                lbUsersList.ItemsSource = lb;
+                case "prev":
+                    pc.CurrentPage--;
+                    break;
+                case "next":
+                    pc.CurrentPage++;
+                    break;
+                default:
+                    pc.CurrentPage = Convert.ToInt32(tb.Text);
+                    break;
             }
-            catch { }
+
+            //определение списка
+            lbUsersList.ItemsSource = listUsers.Skip(pc.CurrentPage * pc.CountPage - pc.CountPage).Take(pc.CountPage).ToList();
+            txtCurentPage.Text = "Текущая страница: " + (pc.CurrentPage).ToString();
+
         }
 
         private void txtPageCount_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
             {
-                listUsers = users.Skip(0).Take(Convert.ToInt32(txtPageCount.Text)).ToList();
-                lbUsersList.ItemsSource = listUsers;
+                pc.CountPage = Convert.ToInt32(txtPageCount.Text);
             }
-            catch { }
+            catch
+            {
+                pc.CountPage = listUsers.Count;
+            }
+            pc.Countlist = users.Count;
+            lbUsersList.ItemsSource = listUsers.Skip(0).Take(pc.CountPage).ToList();
         }
 
         private void btnDLL_Click(object sender, RoutedEventArgs e)
         {
             LoadPages.MainFrame.Navigate(new Pdll());
+        }
+
+        private void UserImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Image IMG = sender as Image;
+            int ind = Convert.ToInt32(IMG.Uid);
+            users U = BaseConnect.BaseModel.users.FirstOrDefault(x => x.id == ind);
+            BitmapImage BI;
+            switch (U.gender)
+            {
+                case 1:
+                    BI = new BitmapImage(new Uri(@"/название/male.jpg", UriKind.Relative));
+                    break;
+                case 2:
+                    BI = new BitmapImage(new Uri(@"/название/female.jpg", UriKind.Relative));
+                    break;
+                default:
+                    BI = new BitmapImage(new Uri(@"/название/other.jpg", UriKind.Relative));
+                    break;
+            }
+            IMG.Source = BI;//помещаем картинку в image
         }
     }
 }
